8b387dd5f818edd54303466b9d00ccc8dc8acf3f
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2022-10-19 04:07:49 +0530

    Solution updated to .NET MAUI SR6

--- src/BlazorApp/BlazorApp.Wasm/Services/DialogService.cs
+++ src/BlazorApp/BlazorApp.Wasm/Services/DialogService.cs
@@ -4,35 +4,40 @@ using VijayAnand.MauiToolkit.Core.Services;
 
 namespace BlazorApp.Wasm.Services
 {
-    public class DialogService : IDialogService
-    {
-        //private readonly IJSInProcessRuntime jsRuntime;
-        private readonly IJSRuntime jsRuntime;
+	public class DialogService : IDialogService
+	{
+		//private readonly IJSInProcessRuntime jsRuntime;
+		private readonly IJSRuntime jsRuntime;
 
-        public DialogService(IJSRuntime jsRuntime)
-        {
-            this.jsRuntime = jsRuntime;
-        }
+		public DialogService(IJSRuntime jsRuntime)
+		{
+			this.jsRuntime = jsRuntime;
+		}
 
-        public Task<string> DisplayActionSheetAsync(string title, string cancel, string destruction, params string[] buttons)
-        {
-            throw new NotImplementedException();
-        }
+		public Task<string> DisplayActionSheetAsync(string title, string cancel, string destruction, params string[] buttons)
+		{
+			throw new NotImplementedException();
+		}
 
-        public async Task DisplayAlertAsync(string title, string message, string cancel)
-        {
-            //await Task.Run(() => jsRuntime.InvokeVoid("showAlert", title, message, cancel));
-            await jsRuntime.InvokeVoidAsync("showAlert", title, message, cancel);
-        }
+		public Task<string> DisplayActionSheetAsync(string title, string message, string cancel, string destruction, string defaultButton, params string[] buttons)
+		{
+			throw new NotImplementedException();
+		}
 
-        public Task<bool> DisplayAlertAsync(string title, string message, string accept, string cancel)
-        {
-            throw new NotImplementedException();
-        }
+		public async Task DisplayAlertAsync(string title, string message, string cancel)
+		{
+			//await Task.Run(() => jsRuntime.InvokeVoid("showAlert", title, message, cancel));
+			await jsRuntime.InvokeVoidAsync("showAlert", title, message, cancel);
+		}
 
-        public Task<string> DisplayPromptAsync(string title, string message, string accept = "OK", string cancel = "Cancel", string placeholder = null, int maxLength = -1, InputType inputType = InputType.Default, string initialValue = "")
-        {
-            throw new NotImplementedException();
-        }
-    }
+		public Task<bool> DisplayAlertAsync(string title, string message, string accept, string cancel)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<string> DisplayPromptAsync(string title, string message, string accept = "OK", string cancel = "Cancel", string placeholder = null, int maxLength = -1, InputType inputType = InputType.Default, string initialValue = "")
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
--- src/BlazorApp/BlazorApp.Wpf/Services/DialogService.cs
+++ src/BlazorApp/BlazorApp.Wpf/Services/DialogService.cs
@@ -3,28 +3,33 @@ using VijayAnand.MauiToolkit.Core;
 
 namespace BlazorApp.Wpf.Services
 {
-    public class DialogService : IDialogService
-    {
-        public Task<string> DisplayActionSheetAsync(string title, string cancel, string destruction, params string[] buttons)
-        {
-            throw new NotImplementedException();
-        }
+	public class DialogService : IDialogService
+	{
+		public Task<string> DisplayActionSheetAsync(string title, string cancel, string destruction, params string[] buttons)
+		{
+			throw new NotImplementedException();
+		}
 
-        public Task DisplayAlertAsync(string title, string message, string cancel)
-        {
-            _ = MessageBox.Show(message, title, MessageBoxButton.OK, MessageBoxImage.Information);
-            return Task.FromResult(true);
-        }
+		public Task<string> DisplayActionSheetAsync(string title, string message, string cancel, string destruction, string defaultButton, params string[] buttons)
+		{
+			throw new NotImplementedException();
+		}
 
-        public Task<bool> DisplayAlertAsync(string title, string message, string accept, string cancel)
-        {
-            var result = MessageBox.Show(message, title, MessageBoxButton.OKCancel, MessageBoxImage.Information);
-            return Task.FromResult(result == MessageBoxResult.OK);
-        }
+		public Task DisplayAlertAsync(string title, string message, string cancel)
+		{
+			_ = MessageBox.Show(message, title, MessageBoxButton.OK, MessageBoxImage.Information);
+			return Task.FromResult(true);
+		}
 
-        public Task<string> DisplayPromptAsync(string title, string message, string accept = "OK", string cancel = "Cancel", string placeholder = null, int maxLength = -1, InputType inputType = InputType.Default, string initialValue = "")
-        {
-            throw new NotImplementedException();
-        }
-    }
+		public Task<bool> DisplayAlertAsync(string title, string message, string accept, string cancel)
+		{
+			var result = MessageBox.Show(message, title, MessageBoxButton.OKCancel, MessageBoxImage.Information);
+			return Task.FromResult(result == MessageBoxResult.OK);
+		}
+
+		public Task<string> DisplayPromptAsync(string title, string message, string accept = "OK", string cancel = "Cancel", string placeholder = null, int maxLength = -1, InputType inputType = InputType.Default, string initialValue = "")
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
